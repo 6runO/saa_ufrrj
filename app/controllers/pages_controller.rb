f24b731265667f82c36365c46dbe234a7bd4fb2b
bruno gonzaga <babsgonzaga@gmail.com>
2022-01-03 22:00:19 -0300

    Added uploaded file checks.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,20 @@ class PagesController < ApplicationController
   end
 
   def upload
+    if params[:historico].blank?
+      redirect_to root_path, notice: "O campo de upload não pode estar vazio."
+    elsif File.extname(params[:historico].path) != ".pdf"
+      redirect_to root_path, notice: "Apenas são aceitos arquivos em formato PDF."
+    elsif (File.size(params[:historico].path).to_f / 2**20).round(3) > 0.300
+      redirect_to root_path, notice: "O arquivo selecionado não pode ter mais que 2 MB (Mega Bytes)."
+    else
+      parse_uploaded_file
+    end
+  end
+
+  private
+
+  def parse_uploaded_file
     @h = Historico.new
     temp = Tempfile.new(['historico', '.csv'])
     @pdf_verified = @h.pdf_is_historico?(params[:historico].path)
@@ -18,8 +32,6 @@ class PagesController < ApplicationController
     end
     temp.unlink
   end
-
-  private
 
   def historico_csv_analysis(csv_path)
     csv = CSV.read(csv_path, headers: true)
