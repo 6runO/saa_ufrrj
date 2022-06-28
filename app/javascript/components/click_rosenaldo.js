d06fae99175e1b5e2e5aba8562f5cf6d22fa349d
brunogonzaga <babsgonzaga@gmail.com>
2022-06-28 01:29:30 -0300

    worked on rosenaldo.

--- app/javascript/components/click_rosenaldo.js
+++ app/javascript/components/click_rosenaldo.js
@@ -1,8 +1,13 @@
-const toggleAverages = () => {
+const clickRosenaldo = () => {
+  const footer = document.querySelector('.page-footer');
+  const rosenaldo = document.querySelector('.rosenaldo');
+
+  rosenaldo.onclick = function () {
+    rosenaldo.classList.add('page-footer-on-click');
+  };
 
 
 
-  
 };
 
-export { toggleAverages };
+export { clickRosenaldo };
