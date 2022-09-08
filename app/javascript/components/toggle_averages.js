bd0a27c0cc4974cbe219aa51b865a052550bb45c
brunogonzaga <babsgonzaga@gmail.com>
2022-09-07 23:23:38 -0300

    media totals

--- app/javascript/components/toggle_averages.js
+++ app/javascript/components/toggle_averages.js
@@ -5,16 +5,27 @@ const toggleAverages = () => {
   const averagesCurso = document.getElementsByClassName('index-data-curso');
   const averagesGeral = document.getElementsByClassName('index-data-geral');
 
+  const totalCurso = document.getElementsByClassName('total-curso');
+  const totalGeral = document.getElementsByClassName('total-geral');
+
   checkCurso.onclick = function () {
     if (checkCurso.checked) {
       for (let i = 0; i < averagesCurso.length; i++) {
         averagesCurso[i].classList.remove("index-data-curso-off");
         averagesCurso[i].classList.add("index-data-curso-on");
       };
+      for (let i = 0; i < totalCurso.length; i++) {
+        totalCurso[i].classList.remove("total-curso-off");
+        totalCurso[i].classList.add("total-curso-on");
+      };
     } else {
       for (let i = 0; i < averagesCurso.length; i++) {
         averagesCurso[i].classList.remove("index-data-curso-on");
         averagesCurso[i].classList.add("index-data-curso-off");
+      };
+      for (let i = 0; i < totalCurso.length; i++) {
+        totalCurso[i].classList.remove("total-curso-on");
+        totalCurso[i].classList.add("total-curso-off");
       };
     };
   };
@@ -25,10 +36,18 @@ const toggleAverages = () => {
         averagesGeral[i].classList.remove("index-data-geral-off");
         averagesGeral[i].classList.add("index-data-geral-on");
       };
+      for (let i = 0; i < totalCurso.length; i++) {
+        totalGeral[i].classList.remove("total-geral-off");
+        totalGeral[i].classList.add("total-geral-on");
+      };
     } else {
       for (let i = 0; i < averagesGeral.length; i++) {
         averagesGeral[i].classList.remove("index-data-geral-on");
         averagesGeral[i].classList.add("index-data-geral-off");
+      };
+      for (let i = 0; i < totalCurso.length; i++) {
+        totalGeral[i].classList.remove("total-geral-on");
+        totalGeral[i].classList.add("total-geral-off");
       };
     };
   };
